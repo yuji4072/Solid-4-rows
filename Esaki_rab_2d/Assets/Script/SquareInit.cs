70b3bab7755ef49ff49209c5d6683bf3e8e27de4
yuji4072 <67566942+yuji4072@users.noreply.github.com>
2020-12-29 03:06:18 +0900

    立体四目並べの試作です。
    プレイヤー側の試作をしたのみで、勝敗判定や観戦画面についてはまだまだわかんないです。今のところできることは、クリックをすると色がプレイヤーの色に変わることと、三次元配列にゲームの状況を格納（キューのように）しただけです。

---
+++ Esaki_rab_2d/Assets/Script/SquareInit.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareInit : MonoBehaviour
+{
+
+    public GameObject SquaresObjprefab;
+    public GameObject SquaresObj;
+
+    int a = 0;
+
+    void Awake()
+    {
+        for (int x = 0; x < 4; x++)
+        {
+            for (int y = 0; y < 4; y++)
+            {
+                GameObject g = Instantiate(SquaresObjprefab, SquaresObj.transform);
+                g.name = "Square" + a;
+                a++;
+                g.transform.position = new Vector2((-3.375f + (2f * x) + (0.25f * x)),  (3.375f + (-2f * y)+ (-0.25f *y)));
+            }
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+}
